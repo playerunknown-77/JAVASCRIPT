177245bf7c68e4ea9f380963a1a145265ebf7a86
Arabinda pradhan <arabindapradhan698@gmail.com>
2026-07-10 10:10:59 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as new version
+
+//alert(3+3) we are using nodeJS not browser
+console.log(3+3)
+console.log("Arabinda")//code readability should be high
+
+let name="Arabinda"
+let age=20
+let isLoggedIn=true
+let state=null
+
+//number=>2 to power 53
+//bigint
+//string=""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+console.log(state)
+
+console.log(typeof name )
+console.log(typeof state)
