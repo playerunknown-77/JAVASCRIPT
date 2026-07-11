3e5b0837fd8ae4801cc39448709a3bf47340c0f0
Arabinda pradhan <arabindapradhan698@gmail.com>
2026-07-11 13:40:58 +0000

    operations

--- basics/03_conversiondatatype.js
+++ basics/03_conversiondatatype.js
@@ -20,8 +20,35 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+
+
+//***********************************Operations********************************** */
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1="Hello"
+let str2=" Arabinda"
+let str3= str1 + str2
+console.log(str3);
+
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+
+
 
 
 
