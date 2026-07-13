8f60c175e6e71534a387f9aff732169f5000d372
Arabinda pradhan <arabindapradhan698@gmail.com>
2026-07-13 17:50:45 +0000

    array_1

---
+++ basics/01_arrays.js
@@ -0,0 +1,35 @@
+const myArr=[0,1,2,3,4,5]
+const myArr2=new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+// myArr.push(6);
+// console.log(myArr);
+// myArr.push(7);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(9);
+// console.log(myArr);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+const newArr=myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+
+//slice & splice
+console.log("A ",myArr);
+
+const mya1=myArr.slice(1,3);
+console.log(mya1);
+
+const mya2=myArr.splice(1,3)
+console.log(mya2);
+
+
+
+
+
+
